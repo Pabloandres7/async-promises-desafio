e699d7b49364c518f721d22bdb1882ec964efaa4
Pablo Granados <pabloandres7@gmail.com>
2021-06-02 02:05:39 -0300

    Hechas las promesas

--- models.ts
+++ models.ts
@@ -1,35 +1,41 @@
 import * as jsonfile from "jsonfile";
 
 class Contact {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 }
 
 class ContactsCollection {
-  data: Contact[] = [];
-  load() {
-    // usar la version Async (readFile)
-    const json = jsonfile.readFileSync("./contacts.json");
-    this.data = json;
-  }
-  getAll() {
-    return this.data;
-  }
-  addOne(contact: Contact) {
-    this.data.push(contact);
-  }
-  save() {
-    // usar la version Async (writeFIle)
-    jsonfile.writeFileSync("./contacts.json", this.data);
-  }
-  getOneById(id) {
-    const encontrado = this.data.find((contacto) => {
-      if (contacto.id == id) {
-        return true;
-      }
-    });
+    data: Contact[] = [];
+    load() {
+        // usar la version Async (readFile)
+        // const json = jsonfile.readFileSync("./contacts.json");
+        const promesa = jsonfile.readFile("./contacts.json");
+        promesa.then((archJson)=> {
+            this.data = archJson;
+            // console.log(this);
+        });
+        // this.data = json;
+        return promesa;
+    }
+    getAll() {
+        return this.data;
+    }
+    addOne(contact: Contact) {
+        this.data.push(contact);
+    }
+    save() {
+        // usar la version Async (writeFIle)
+        jsonfile.writeFile("./contacts.json", this.data);
+    }
+    getOneById(id) {
+        const encontrado = this.data.find((contacto) => {
+            if (contacto.id == id) {
+                return true;
+            }
+        });
 
-    return encontrado;
-  }
+        return encontrado;
+    }
 }
 export { ContactsCollection, Contact };
